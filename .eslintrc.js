c1351903f8b147fc00c03ae660e61eb5cf60e0b4
Erik Olsen <erawk26@gmail.com>
2019-11-03 23:30:34 -0500

    formatting fixes / eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,23 @@ module.exports = {
     browser: true,
     node: true
   },
+  globals: {
+    $nuxt: true
+  },
   parserOptions: {
     parser: 'babel-eslint'
   },
   extends: [
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
   ],
   plugins: ['prettier'],
-//   // add your custom rules here
-  rules: { 'no-console': 'off' }
+  //   // add your custom rules here
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  }
 }
