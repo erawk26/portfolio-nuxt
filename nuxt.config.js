692e82f6e5f84698a970e4f498500b14409ac71d
Erik Olsen <erawk26@gmail.com>
2019-11-06 18:34:27 -0500

    better router base

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,15 @@
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/portfolio-nuxt/'
+        }
+      }
+    : {}
+
 export default {
-  router: {
-    base: './'
-  },
+  ...routerBase,
   mode: 'universal',
   /*
    ** Headers of the page
