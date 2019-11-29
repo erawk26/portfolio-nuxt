084dc137a8ad2ac52926bb6c93d598abaaa6b9a7
Erik Olsen <erawk26@gmail.com>
2019-11-29 12:50:34 -0500

    missing array around vuetify module in the nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,46 +49,48 @@ export default {
    */
   modules: [
     [
-      '@nuxtjs/vuetify',
-      {
-        theme: {
-          themes: {
-            dark: {
-              primary: '#263238',
-              accent: '#C67171',
-              secondary: '#26A5B5',
-              success: '#4CAF50',
-              info: '#2196F3',
-              warning: '#FB8C00',
-              error: '#FF5252',
-              dkGreen: '#3b8070',
-              charcoal: '#342a21'
-            },
-            light: {
-              primary: '#757575',
-              accent: '#F44336',
-              secondary: '#0277BD',
-              success: '#4CAF50',
-              info: '#2196F3',
-              warning: '#FB8C00',
-              error: '#FF5252',
-              dkGreen: '#3b8070',
-              charcoal: '#342a21'
+      '@nuxtjs/style-resources',
+      'nuxt-validate',
+      '@nuxtjs/eslint-module',
+      '@nuxtjs/google-analytics',
+      [
+        '@nuxtjs/vuetify',
+        {
+          theme: {
+            themes: {
+              dark: {
+                primary: '#263238',
+                accent: '#C67171',
+                secondary: '#26A5B5',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252',
+                dkGreen: '#3b8070',
+                charcoal: '#342a21'
+              },
+              light: {
+                primary: '#757575',
+                accent: '#F44336',
+                secondary: '#0277BD',
+                success: '#4CAF50',
+                info: '#2196F3',
+                warning: '#FB8C00',
+                error: '#FF5252',
+                dkGreen: '#3b8070',
+                charcoal: '#342a21'
+              }
             }
-          }
-        },
-        defaultAssets: {
-          font: {
-            family: 'Libre Baskerville'
           },
-          icons: 'mdi'
+          defaultAssets: {
+            font: {
+              family: 'Libre Baskerville'
+            },
+            icons: 'mdi'
+          }
         }
-      }
-    ],
-    'nuxt-validate',
-    '@nuxtjs/eslint-module',
-    '@nuxtjs/google-analytics',
-    '@nuxtjs/style-resources'
+      ]
+    ]
   ],
   googleAnalytics: {
     id: 'UA-115116018-1'
